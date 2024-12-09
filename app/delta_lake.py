797a5d36845cac1f225f26d0fd8ddfc64279ce94
Steve Lin <company@globalinvestai.com>
2024-12-09 09:20:18 -0500

    init

--- app/delta_lake.py
+++ app/delta_lake.py
@@ -1,4 +1,12 @@
-from deltalake import DeltaTable
+from deltalake import DeltaTable, write_deltalake
+import pandas as pd
 
-dt = DeltaTable("./deltatbl-partitioned")
-dt.files()
+# write some data into a delta table
+df = pd.DataFrame({"id": [1, 2], "value": ["foo", "boo"]})
+write_deltalake("./data/delta", df)
+
+# Load data from the delta table
+dt = DeltaTable("./data/delta")
+df2 = dt.to_pandas()
+
+assert df.equals(df2)
